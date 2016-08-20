e9b2a0f9cf38284c38ce15ac6aa6360fbbdd920b
brewideas <ashugid@gmail.com>
2016-08-20 10:45:11 +0530

    Campaign edit

--- app/src/main/java/com/fourway/ideaswire/ui/CreateCampaign_EnterData.java
+++ app/src/main/java/com/fourway/ideaswire/ui/CreateCampaign_EnterData.java
@@ -1,5 +1,6 @@
 package com.fourway.ideaswire.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -21,7 +22,8 @@ public class CreateCampaign_EnterData extends AppCompatActivity {
 
     public void uploadToCreateCampaign(View view) {
 
-
+        Intent intent = new Intent(this,EditPhotoSelectedUi.class);
+        startActivity(intent);
 
     }
 
