55c9bb33bd7d7b6623a97422c81ea5e37d789b31
brewideas <ashugid@gmail.com>
2016-09-25 19:10:13 +0530

    Activity for search results

---
+++ app/src/main/java/com/fourway/ideaswire/ui/searchResults.java
@@ -0,0 +1,33 @@
+package com.fourway.ideaswire.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+import com.fourway.ideaswire.R;
+
+public class searchResults extends AppCompatActivity {
+
+    SearchProfileListAdapter msearchProfileAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serach_results);
+
+        GridView gv = (GridView) findViewById(R.id.searchResultsGridView);
+
+        /*msearchProfileAdapter = new SearchProfileListAdapter(this, loginUi.mProfileList);
+
+        gv.setAdapter(msearchProfileAdapter);
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+*/
+
+    }
+
+}
